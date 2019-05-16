5c57f484baadb93c4dd8bfe7ade84a9232c233f9
miha1997 <mihailo.obradovic1997@gmail.com>
2019-05-16 22:06:35 +0200

    Update SingleAuction.php

--- Implementation/application/controllers/SingleAuction.php
+++ Implementation/application/controllers/SingleAuction.php
@@ -6,6 +6,22 @@
             parent::__construct();
 
             $this->load->model('Auction');
+            $this->load->model('UserBids');
+
+            $id = $this->input->get("id");
+            $auction = $this->Auction->getAuctionById($id)[0];
+
+            if($auction == null){
+                redirect("InfoMessage/PageNotFound");
+            }
+
+            $logged_user = $this->session->userdata("user");
+
+            if($logged_user == null || $logged_user->user_rank == 0){
+                if($auction->auction_state == 'Denied' || $auction->auction_state == 'Pending confirmation')
+                    redirect("InfoMessage/PageNotFound");
+
+            }
 
         }
 
@@ -26,13 +42,38 @@
             $auction = $this->Auction->getAuctionById($id)[0];
             $bid = $this->Auction->getMaxBid($id);
 
-            if($auction == null){
-                redirect("InfoMessage/PageNotFound");
-            }
-
             $content["auction"] = $auction;
             $content["bid"] = $bid;
             $this->loadPageLayout("pages/SingleAuction.php", $content);
+
+        }
+
+        public function Bid(){
+            $old_value = $this->input->post("old_bid_value");
+            $new_bid = $this->input->post("bid");
+
+            if($old_value == null)
+                redirect("InfoMessage/PageNotFound");
+
+            $id = $this->input->get("id");
+            $auction = $this->Auction->getAuctionById($id)[0];
+            $bid = $this->Auction->getMaxBid($id);
+
+            if($bid->bid_value != $old_value){
+                redirect(base_url() .'InfoMessage/AuctionChanged?id=' . $auction->auction_id);
+            }
+
+            $bid_time = date("Y-m-d H:i:s");
+            $logged_user = $this->session->userdata("user")->username;
+
+            $this->UserBids->createBid($auction->auction_id, $logged_user, $bid_time, $new_bid);
+            
+            redirect(base_url() .'InfoMessage/BidSuccessful?id=' . $auction->auction_id);
+            //redirect(base_url() . 'SingleAuction?id=' . $auction->auction_id);
+
+            //$content["auction"] = $auction;
+            //$content["bid"] = $bid;
+            //$this->loadPageLayout("pages/SingleAuction.php", $content);
 
         }
 
